4146ffbb453089f9a8ccc618082adc4549b973f9
dufuyao1102 <33566608+dufuyao1102@users.noreply.github.com>
2020-05-25 00:14:58 -0500

    Update Random Forest.py

--- Random Forest.py
+++ Random Forest.py
@@ -16,15 +16,30 @@ train_ans = pd.read_csv('./train_answers.csv')
 train_ans = train_ans.iloc[:,1]
 y = np.repeat(train_ans.to_numpy(), 4)
 
-train_tort_data = train_data.iloc[:, 4:44]
+train_chain_data = train_data.iloc[:, 901:5020]
+x = train_chain_data.to_numpy()
 
-x = train_tort_data.to_numpy()
 
 model = RandomForestClassifier()
 model.fit(X=x, y=y)
-
 answers = model.predict(X=x)
-
 print((answers != y).sum())
-print(answers[:32])
-print(y[:32])
+
+
+chaincode_features = (train_data.iloc[:, 901:5020]).to_numpy()
+test_idx = np.arange(3, 1128, 4)
+train_idx = np.delete(np.arange(1128), test_idx)
+
+x_test = chaincode_features[test_idx, :]
+x_train = chaincode_features[train_idx, :]
+y_train = np.repeat(train_ans.to_numpy(), 3)
+
+y_test = train_ans
+model.fit(X=x_train, y=y_train)
+
+
+print(model.score(X=x_train, y=y_train))
+print((model.predict(X=x_train) != y_train).sum())
+print('\n')
+print(model.score(X=x_test, y=y_test))
+print((model.predict(X=x_test) != y_test).sum())
